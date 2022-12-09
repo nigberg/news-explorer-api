a576011336b7eaa96925f760dbe3c9512d8b659e
nigberg <nigberg@gmail.com>
2022-12-09 12:25:02 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { SECRET_KEY } = require('./utils/constants')
+const { SECRET_KEY, MONGO_SERVER_ADDRESS } = require('./utils/constants')
 const { PORT = 3000 } = process.env
 const SECRET_JWT =
   process.env.NODE_ENV !== 'production' ? SECRET_KEY : process.env.SECRET_JWT
@@ -8,14 +8,20 @@ const usersRouter = require('./routes/users')
 const articlesRouter = require('./routes/articles')
 const helmet = require('helmet')
 const cors = require('cors')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 const NotFoundError = require('./utils/errors/NotFoundError')
 const auth = require('./middlewares/auth')
 const {createUser, login} = require('./controllers/users')
+
 const app = express()
+mongoose.connect(MONGO_SERVER_ADDRESS)
 
 app.use(cors())
 app.options('*', cors())
-app.use(helmet)
+app.use(helmet())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.post('/signup', createUser);
 app.post('/signin', login);
 app.use(auth);
